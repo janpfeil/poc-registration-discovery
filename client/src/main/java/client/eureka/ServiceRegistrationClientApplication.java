6ec9751cff6a00d692af56b05313dfc68df06fb6
obeidi <jobsobeidi@gmail.com>
2021-04-28 01:29:33 +0200

    update Architectur of the Client Application

---
+++ client/src/main/java/client/eureka/ServiceRegistrationClientApplication.java
@@ -0,0 +1,38 @@
+package client.eureka;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableEurekaClient
+public class ServiceRegistrationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistrationClientApplication.class, args);
+	}
+}
+
+//@RestController
+//class ServiceInstanceRestController {
+//
+//	@Autowired
+//	private DiscoveryClient discoveryClient;
+//
+//	@RequestMapping("/service-instances/{applicationName}")
+//	public List<ServiceInstance> serviceInstancesByApplicationName(
+//			@PathVariable String applicationName) {
+//		System.out.println(applicationName);
+//		return this.discoveryClient.getInstances(applicationName);
+//	}
+//}
