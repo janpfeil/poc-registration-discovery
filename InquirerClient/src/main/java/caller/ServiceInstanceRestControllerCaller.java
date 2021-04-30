1f06e6fbdebf8259ea6daa54d96c9047c88d10f9
obeidi <jobsobeidi@gmail.com>
2021-04-30 16:05:40 +0200

    add OpenApi-Swagger Documentation

--- InquirerClient/src/main/java/caller/ServiceInstanceRestControllerCaller.java
+++ InquirerClient/src/main/java/caller/ServiceInstanceRestControllerCaller.java
@@ -7,19 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-//@EnableEurekaClient
 @RestController
+@RequestMapping("/client")
 class ServiceInstanceRestController {
 
 	@Autowired
 	private DiscoveryClient discoveryClient;
 
 	@RequestMapping("/service-instances/{applicationName}")
+	@GetMapping("/service-instances/{applicationName‚}")
 	public List<ServiceInstance> serviceInstancesByApplicationName(
 			@PathVariable String applicationName) {
 		System.out.println(applicationName);
